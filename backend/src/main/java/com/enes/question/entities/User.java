6c10ba8f1f5d3b207968bd674e28576591f5b0a2
Enes Hakan <96254574+enshkn@users.noreply.github.com>
2024-07-31 20:43:43 +0300

    testing new branch

--- backend/src/main/java/com/enes/question/entities/User.java
+++ backend/src/main/java/com/enes/question/entities/User.java
@@ -1,13 +1,11 @@
-public
 package com.enes.question.entities;
-
 import lombok.Data;
-
 @Data
 class User {
 
     Long id;
     String userName;
     String password;
+    // user class has a role
 
 }
